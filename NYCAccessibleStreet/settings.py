9894991f4e7a5e764a8bdf62d40bd812e1720bcb
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2023-11-08 00:54:30 -0500

    initial

--- NYCAccessibleStreet/settings.py
+++ NYCAccessibleStreet/settings.py
@@ -84,15 +84,18 @@ WSGI_APPLICATION = "NYCAccessibleStreet.wsgi.application"
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": config("dbname"),
-        "USER": config("dbuser"),
-        "PASSWORD": config("dbpassword"),
-        "HOST": config("dbhost"),
-        "PORT": "3306",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
